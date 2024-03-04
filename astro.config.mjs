ad09704fa4c70aeb7c3aa74b811f5214f3966891
Richard Wang <git@xmail.ing>
2024-03-04 07:01:45 +0000

    add synology-dsm.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,8 @@ export default defineConfig({
                                 'VM & Linux',
                             items:
                                 [{label: 'Proxmox PVE', link: '/selfhosted/vm/proxmox-pve'},
-                                    {label: 'CUDA 虚拟机环境', link: '/selfhosted/vm/cuda-vm'}
+                                    {label: 'CUDA 虚拟机环境', link: '/selfhosted/vm/cuda-vm'},
+                                    {label: '黑群晖', link: '/selfhosted/vm/synology-dsm'},
                                 ]
                         },
                         {
